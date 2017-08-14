f7fa92a64b966e35c401cf445b50b6d62497b4be
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2017-08-14 12:43:11 -0400

    working UI for judging dialog and presenting the entries

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-/* globals: Handlebars, $, firebase */
+/* globals: Handlebars, _, firebase */
+
+// Register a helper
+Handlebars.registerHelper('capitalize', function(str){
+  // str is the argument passed to the helper when called
+  str = str || '';
+  return str.slice(0,1).toUpperCase() + str.slice(1);
+});
 
 const appConfig  = {
-  appTitle: "CookOff"
+  appTitle: "CookOff",
+
+  scorecard: [
+    { name: "taste", min: 0, max: 10},
+    { name: "texture", min: 0, max: 10},
+    { name: "smell", min: 0, max: 10},
+    { name: "presentation", min: 0, max: 0},
+    { name: "expected", min: 0, max: 10},
+    { name: "innovative", min: 0, max: 10}
+  ]
 };
+
 
 const contestEntries = [
   {
@@ -67,11 +84,37 @@ entryDialog.listen('MDCDialog:accept', function() {
 entryDialog.listen('MDCDialog:cancel', function() {
   console.log('canceled');
 });
+//_. displayRatingEntry
+function displayRatingEntry(id, entries) {
+  // find entry in the list
+  let entry = _.find(entries, (e) => e.id === id);
+  
+  // find and compile Handlebars template
+  let judgeEntrySource   = document.getElementById("judge-entry-template").innerText;
+  let judgeEntryTemplate = Handlebars.compile(judgeEntrySource);
+  // if entry has a scorecard for the current user show it
+  // if entry doesn't hanve a scrorecard for the current user - use default one
+  if( ! entry.scorecard ) {
+    entry.scorecard = appConfig.scorecard;
+  }
+  let entryHtml = judgeEntryTemplate(entry);
+  // clear up previous content and add a new one
+  _.each(document.querySelectorAll('.judge-entry-dialog'), c => document.getElementById("rate-contestant").removeChild(c));
+  document.getElementById("rate-contestant").insertAdjacentHTML('beforeend',entryHtml);
 
-//_. Sliders
-const {MDCSlider} = mdc.slider;
-const slider = new MDCSlider(document.querySelector('#judge-slider'));
-slider.listen('MDCSlider:change', () => console.log(`Value changed to ${slider.value}`));
+//_ , Sliders
+  const {MDCSlider} = mdc.slider;
+  _.each(entry.scorecard, (e) => {
+    const slider = new MDCSlider(document.querySelector(`#judge-slider-${e.name}`));
+    slider.listen('MDCSlider:change', (evt) => {
+      document.querySelector(`#judge-slider-${e.name}-value`).innerText = `${slider.value}`;
+      console.log(`Value ${e.name} changed to ${slider.value}`);
+      console.log(`Details: ${evt}`);
+    });
+  });
+ 
+  entryDialog.show();
+}
 
 //_ Authentication
 const authDialog = new mdc.dialog.MDCDialog(document.querySelector('#auth-dialog'));
@@ -104,8 +147,8 @@ function displayEntries (data) {
       // now we need to figure out which item user clicked on
       if(e.currentTarget && e.currentTarget.nodeName == "LI") {
         //console.log(e.currentTarget.id + " was clicked");
-        entryDialog.lastFocusedTarget = e.target;
-        entryDialog.show();
+        entryDialog.lastFocusedTarget = e.currentTarget;
+        displayRatingEntry(e.currentTarget.id, contestEntries);
         // HACK: this is to show sliders on a dialog correctly
         setTimeout(function(e){
           let event = document.createEvent('HTMLEvents');
