f1fc89cb8a3b583ea798cbb78167a580b9f0952c
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2017-08-15 16:06:19 -0400

    added image scaling

--- public/app.js
+++ public/app.js
@@ -309,11 +309,12 @@ CookOffContest.prototype.updateScorecard = function (e) {
 
 //_. addEntry
 CookOffContest.prototype.addEntry = function() {
-  let currentUser = this.auth.currentUser; 
-  let file = this.newEntryImage.files[0];
+  let currentUser = this.auth.currentUser;
+  let fileName = this.newEntryImage.files[0].name;
+  let file = this.getScaledImageBlob();
  
   // upload image to cloud 
-  let filePath = `${currentUser.uid}/${"test"}/${file.name}`;
+  let filePath = `${currentUser.uid}/${"test"}/${fileName}`;
 
   // upload entry with correct url
   this.db.ref().child('entries').push({
@@ -339,6 +340,16 @@ CookOffContest.prototype.addEntry = function() {
   });
 };
 
+// extract scaled blob of image from canvas
+CookOffContest.prototype.getScaledImageBlob = function() {
+  let arr = this.newEntrySnapshot.toDataURL("image/jpeg").split(','), mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+  while(n--){
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new Blob([u8arr], {type:mime});  
+}
+
 //_. updateNewEntryImage
 CookOffContest.prototype.updateNewEntryImage = function (event) {
   event.preventDefault();
@@ -346,7 +357,33 @@ CookOffContest.prototype.updateNewEntryImage = function (event) {
   if( file != null ) {
     let reader = new FileReader();
     reader.onload = (e) => {
-      this.newEntrySnapshot.src = e.target.result
+      let img = document.createElement('img');
+      img.src = e.target.result;
+      img.onload = (e) => {
+        let canvas = this.newEntrySnapshot;
+        
+        // scale an image
+        var MAX_WIDTH = 800;
+        var MAX_HEIGHT = 600;
+        var width = img.width;
+        var height = img.height;
+        
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+          }
+        }
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+      }
     };
     reader.readAsDataURL(file);
   }
